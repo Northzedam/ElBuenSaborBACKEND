76bfb6dbaf236174ae028388a9c886f21f896071
Luciano <lucho9206@hotmail.com>
2020-11-28 13:26:28 -0300

    Control de usuarios

---
+++ src/main/java/com/example/demo/repository/SecurityRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.User;
+import com.sun.el.stream.Optional;
+
+@Repository
+public interface SecurityRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT u FROM User u WHERE u.uid = :uid")
+	public User findByUid(long uid);
+}
